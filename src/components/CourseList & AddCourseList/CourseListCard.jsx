3756ebb52a64c557aa0f11f9aac6d27f3e1be7aa
CedrickDale <cdalesunga@gmail.com>
2024-01-29 15:09:41 +0800

    addcoursenew

--- src/components/CourseList & AddCourseList/CourseListCard.jsx
+++ src/components/CourseList & AddCourseList/CourseListCard.jsx
@@ -1,7 +1,6 @@
 /*January 10, 2024*/
 import React, { useState, useEffect } from "react";
 import { IoAdd } from "react-icons/io5";
-
 import axios from "axios";
 
 import { Link } from "react-router-dom";
@@ -25,17 +24,8 @@ const CourseListCard = () => {
     const result = await axios.get("http://localhost:8080/getCourse");
     setCourses(result.data);
   };
-  /*January 15 2024 */
 
-  //course list mock data
   const { courselist } = data;
-  return (
-    <>
-      {/* 1/12/2024 UI development and Mobile responsiveness */}
-
-      <div className="">
-        {/* 1/15/2024 functions and buttons */}
-        <div className="">
        
           <div className=" xl:w-[1244px] w-[90%] mt-10 flex mx-auto flex-col lg:center-row lg:w-[50%] lg:m-auto lg:mt-5 items-center gap-5">
             {/*January 15 2024, API connection of frontend to backend can fetch data from the backend*/}
@@ -45,27 +35,15 @@ const CourseListCard = () => {
             </div>
             {courselist.map((course, idx) => {
               return (
-                <div key={idx} className="w-full mb-5  rounded-md shadow-md">
-                  <div className="flex px-0 py-0 rounded-md xl:h-[115px]">
-                    <div className="bg-[#BCE8B1] flex py-1 item-center justify-center text-center text-[.8rem] lg:text-[1rem] w-[30%] lg:w-[20%] lg:p-5 rounded-l-sm lg:rounded-l-md">
-                      <p className="lg:font-medium">PL00{course.id}</p>
-                    </div>
-
-                    <p
-                      className="text-white lg:font-bold text-[.8rem] py-1 lg:py-0 lg:text-[1.2rem] w-full flex justify-center items-center
-                   rounded-r-sm lg:rounded-r-md 	bg-[#126912] "
-                    >
-                      {course.courseTitle}
-                    </p>
-                  </div>
-                </div>
-              );
-            })}
+    <>
+      <div className="w-full">
+        <div className="">
+          <div className="xl:w-[1244px] w-[90%] mt-10 flex flex-col lg:w-[50%] lg:mt-5 items-start gap-5">
+            <Link to="/courseoverview">
+              <div className="text-black lg:font-bold text-[.8rem] py-5 lg:py-0 lg:text-[1.2rem] w-full flex justify-start">
+                <p className=" ml-5 lg:font-bold text-[34px] text-left">Course List</p>
+              </div>
             </Link>
-      
-
-            {/*January 15 2024*/}
-            {/*January 19 2024 -gem modify buttons add footer*/}
 
             <Link to="/AddNewCourse">
               <div
@@ -75,9 +53,6 @@ const CourseListCard = () => {
                   <span>
                     <IoAdd className="lg:text-[2rem] text-white" />
                   </span>
-                </div>
-                <div className="bg-[#126912] text-white lg:font-bold h-[5vh] lg:h-[8vh]  w-[50%] flex items-center justify-center rounded-r-sm  lg:rounded-r-md">
-                  <span className="lg:text-[1rem]">Add New Course</span>
                 </div>
               </div>
             </Link>
@@ -90,8 +65,6 @@ const CourseListCard = () => {
             </div>
           </footer>
         </div>
-          {/*January 19 2024 -gem modify buttons add footer*/}
-
       </div>
     </>
   );
